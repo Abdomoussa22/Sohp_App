cefb1a71df243419246a8d1cc6739f238bfd2171
abdelhamid <abdomousa356@gmail.com>
2022-12-10 06:01:24 +0200

    Updates

---
+++ precache-manifest.9d4f38282b0628f2d152b715ca1f7b6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "539c3e4aecbdcbfd4741672c420482b2",
+    "url": "/Sohp_App/index.html"
+  },
+  {
+    "revision": "0eeb46166052152263a8",
+    "url": "/Sohp_App/static/js/2.8b1f522a.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/Sohp_App/static/js/2.8b1f522a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6ac0fb32ab608fa2ec1",
+    "url": "/Sohp_App/static/js/main.62a29f46.chunk.js"
+  },
+  {
+    "revision": "170e5ad9274da5edac63",
+    "url": "/Sohp_App/static/js/runtime-main.c1653153.js"
+  },
+  {
+    "revision": "422fec1079cce450353e924288f8313b",
+    "url": "/Sohp_App/static/media/commerce.422fec10.png"
+  }
+]);
